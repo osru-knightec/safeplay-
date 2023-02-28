d51a51fe38ffc18132a6d22ec76ea97d35140acf
Oskar Rubensson <107415465+osru-knightec@users.noreply.github.com>
2023-02-28 14:48:22 +0100

    Contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Home } from "./pages/Home/Home";
+import { Contact } from "./pages/Contact/Contact";
 import { Privacy } from "./pages/Privacy/Privacy";
 
 function RouteList() {
   return (
     <Routes>
+      <Route path="/contact" element={<Contact />} />
       <Route path="/privacy" element={<Privacy />} />
-      <Route path="/" element={<Privacy />} />
     </Routes>
   );
 }
 
 function App() {
   return (
-    <BrowserRouter basename="/safeplayplus">
+    <HashRouter>
       <RouteList />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
---
+++ src/pages/Contact/Contact.tsx
@@ -0,0 +1,11 @@
+export const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        If you want support or have any questions, please contact us at{" "}
+        <a href="mailto:oskar231@hotmail.com">oskar231@hotmail.com</a>
+      </p>
+    </div>
+  );
+};
