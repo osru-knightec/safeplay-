dda9922bd44550ca5b36a348e9b0ec2289dea70c
Oskar Rubensson <107415465+osru-knightec@users.noreply.github.com>
2023-03-01 12:45:49 +0100

    Updated policy

--- src/pages/Privacy/Privacy.tsx
+++ src/pages/Privacy/Privacy.tsx
@@ -1,4 +1,7 @@
 import html from "../../html/policy";
+
 export const Privacy = () => {
-  return <div style={{ padding: 10 }} dangerouslySetInnerHTML={{ __html: html }}></div>;
+  return (
+    <div style={{ padding: 10, textAlign: "left", display: "block" }} dangerouslySetInnerHTML={{ __html: html }}></div>
+  );
 };
